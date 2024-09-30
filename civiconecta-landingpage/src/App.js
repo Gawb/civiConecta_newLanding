acd06b32f9e7a22c7ba44fea01ac0ee0df4803d3
gweibel <georg.weibel@gmail.com>
2024-09-30 13:40:42 -0300

    adding changes in nav elements

--- civiconecta-landingpage/src/App.js
+++ civiconecta-landingpage/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import { Nav } from './Nav';
+import { Header } from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <Nav />
+      <Header />
     </div>
   );
 }
