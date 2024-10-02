d67b0a4379ca8d3cff4ac19cb2a54f8c117032fe
gweibel <georg.weibel@gmail.com>
2024-10-02 12:09:13 -0300

    adding a sub menu on desktop version

--- civiconecta-landingpage/src/App.js
+++ civiconecta-landingpage/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 import { Header } from './components/Header';
 
+const elements = ['Inicio', 'Sobre Civi', 'Profesionales', 'Testimonios', 'Contacto'];
+const imgArrow = (<svg
+  className="icon-arrow"
+  xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 0 120 120"
+>
+  <path
+    d="M83.9,67.6c1.5,1.5,1.5,3.8,0,5.3l-3.5,3.5c-1.5,1.5-3.8,1.5-5.3,0L60,61.4L44.9,76.4c-1.5,1.5-3.8,1.5-5.3,0
+  l-3.5-3.5c-1.5-1.5-1.5-3.8,0-5.3l21.2-21.2c1.5-1.5,3.8-1.5,5.3,0L83.9,67.6z"
+  />
+</svg>)
+
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Header elements={elements} imgArrow={imgArrow} />
     </div>
   );
 }
--- civiconecta-landingpage/src/components/Header/index.js
+++ civiconecta-landingpage/src/components/Header/index.js
@@ -1,34 +1,59 @@
-import { useState, useEffect } from 'react';
+import './index.css';
+import { useState, useEffect, useRef } from 'react';
 import logo from '../../logos/Logo---CiviConecta---Principal-No-Claim---1000x500.png';
 import { Nav } from './Nav';
 import { MobileNav } from './MobileNav';
 
-const Header = () => {
-    const [isMobile, setIsMobile] = useState(window.innerWidth <= 500);
+const widthScreen = 498;
+
+const Header = ({ elements, imgArrow }) => {
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= widthScreen);
+    const [isHovered, setIsHovered] = useState(false);
+    const menuRef = useRef(false);
+
     const handleResize = () => {
-        setIsMobile(window.innerWidth <= 500);
+        setIsMobile(window.innerWidth <= widthScreen);
     };
 
-    const elements = ['Inicio', 'Sobre Civi', 'Profesionales', 'Testimonios', 'Contacto'];
+    useEffect(() => {
+        const handleMouseEnter = () => {
+            setIsHovered(true);
+        };
+        const handleMouseLeave = () => {
+            setIsHovered(false);
+        };
+        const onMenu = menuRef.current;
 
-    const liElements = (elements) => {
-        return (elements.map((element, index) => (
+        onMenu.addEventListener('mouseenter', handleMouseEnter);
+        onMenu.addEventListener('mouseleave', handleMouseLeave);
+
+        return () => {
+            onMenu.removeEventListener('mouseenter', handleMouseEnter);
+            onMenu.removeEventListener('mouseleave', handleMouseLeave);
+        };
+    }, []);
+
+    const liElements = (elements) => (
+        elements.map((element, index) => (
             <li key={index} id={`element-${index}`}>
-                {element}
-                {index === 1 && (imgArrow)}
+                {index === 1 ? (
+                    <div ref={menuRef}>
+                        <div>
+                            {element} {imgArrow}
+                        </div>
+                        {isHovered
+                            ? <ul ref={menuRef}>
+                                <li>Nuestro Servicio</li>
+                                <li>Ventajas</li>
+                            </ul>
+                            : ''}
+                    </div>
+                ) : (
+                    element
+                )}
             </li>
-        )));
-    };
-    const imgArrow = (<svg
-                    className="icon-arrow"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 120 120"
-                >
-                <path
-                    d="M83.9,67.6c1.5,1.5,1.5,3.8,0,5.3l-3.5,3.5c-1.5,1.5-3.8,1.5-5.3,0L60,61.4L44.9,76.4c-1.5,1.5-3.8,1.5-5.3,0
-                    l-3.5-3.5c-1.5-1.5-1.5-3.8,0-5.3l21.2-21.2c1.5-1.5,3.8-1.5,5.3,0L83.9,67.6z"
-                />
-                </svg>)
+        ))
+    );
 
     const sharedProps = {
         elements,
@@ -46,7 +71,7 @@ const Header = () => {
 
     return (
         <>
-            {isMobile ? <MobileNav {...sharedProps} /> : <Nav {...sharedProps}/>}
+            {isMobile ? <MobileNav {...sharedProps} /> : <Nav {...sharedProps} />}
         </>
     );
 };
